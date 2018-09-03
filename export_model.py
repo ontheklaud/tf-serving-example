4696d5d4ac831e084c4f7bbec139b6a587b533a9
Seungkyun Hong <xo@kisti.re.kr>
2018-09-03 17:27:18 +0900

    revised model export with experimental predict signature & json for RESTful API

--- export_model.py
+++ export_model.py
@@ -32,27 +32,45 @@ def main():
     # tensor wrapping
     regression_input = tf.saved_model.utils.build_tensor_info(X)
     regression_output = tf.saved_model.utils.build_tensor_info(net)
-    regression_loss = tf.saved_model.utils.build_tensor_info(loss)
+    # regression_loss = tf.saved_model.utils.build_tensor_info(loss)
 
     # signature mapping
     regression_signatures = (
         tf.saved_model.signature_def_utils.build_signature_def(
             inputs=dict({
-                'input': regression_input
+                tf.saved_model.signature_constants.REGRESS_INPUTS:
+                    regression_input
             }),
             outputs=dict({
-                'output': regression_output,
-                'loss': regression_loss
+                tf.saved_model.signature_constants.REGRESS_OUTPUTS:
+                    regression_output,
+                # 'loss': regression_loss
             }),
             method_name=
                 tf.saved_model.signature_constants.REGRESS_METHOD_NAME
         )
     )
 
+    # prediction signature (?)
+    tensor_info_x = tf.saved_model.utils.build_tensor_info(X)
+    tensor_info_y = tf.saved_model.utils.build_tensor_info(net)
+
+    prediction_signatures = tf.saved_model.signature_def_utils.build_signature_def(
+        inputs={'x': tensor_info_x},
+        outputs={'y': tensor_info_y},
+        method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME
+    )
+
+    legacy_init_op = tf.group(tf.tables_initializer(), name='legacy_init_op')
+
     builder.add_meta_graph_and_variables(
         sess=sess, tags=[tf.saved_model.tag_constants.SERVING],
-        signature_def_map=dict({'regression_value': regression_signatures}),
-        main_op = tf.tables_initializer(),
+        signature_def_map=dict({
+            'predict': prediction_signatures,
+            'regression_value': regression_signatures
+        }),
+        legacy_init_op=legacy_init_op,
+        # main_op = tf.tables_initializer(),
         strip_default_attrs=True,
     )
 
